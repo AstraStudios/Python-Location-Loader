ae95937a4701e78067f86a5c9b34651fa0c87f66
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-06-16 14:10:06 -0500

    Random password
    Not really a location loader dont know why its named that

--- execute.py
+++ execute.py
@@ -1,1 +1,17 @@
-print("Hello world")
+from random import choice
+import random as random
+
+print("This is a app for a random password")
+
+digits = '1234567890'
+chars = 'qwertyuiopasdfghjklzxcvbnm'
+up = chars.upper()
+special = '!@#$%^&*()-=_+[]\|;:<>,./?'
+
+all = (str(digits + special) + chars + up)
+
+password = ''.join(
+    choice(all) for i in (10)
+)
+
+print(password)
